554a4c93241ba3d3e3d660e222b3f014554500a7
dixon <dixonbegay@gmail.com>
2018-08-01 15:48:27 -0700

    Cleaned up code to get desired output

--- main.go
+++ main.go
@@ -1,37 +1,64 @@
 package arma
 
 import (
-	"runtime"
+	"os/exec"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 )
 
 var configs = "configs"
-var platform string
 
 var log = logrus.New()
 
 // Server is main object to interact with an Arma server
 type Server struct {
+	// Path is the path to the ARMA directory. Not the path to the executable
 	Path string
+
+	// Admins is a list of strings of steamID's
+	Admins []string
+
+	// Platform is either linux, windows, or wine
+	Platform string
 }
 
-func getPlatform() {
+// func getPlatform() {
 
-	switch runtime.GOOS {
+// 	switch runtime.GOOS {
+// 	case "windows":
+// 		platform = "windows"
+// 	case "linux":
+// 		platform = "linux"
+// 	}
+
+// 	if platform == "" {
+// 		log.Fatal("Your operating system doesn't support Arma")
+// 	}
+
+// 	return platform
+// }
+
+// Start an Arma server
+func (s *Server) Start() ([]byte, error) {
+
+	var args []string
+	var armaExecutable string
+
+	switch s.Platform {
 	case "windows":
-		platform = "windows"
+		armaExecutable = strings.Join([]string{s.Path, "arma3server.exe"}, "\\")
+	case "wine":
+		armaExecutable = strings.Join([]string{s.Path, "arma3server.exe"}, "/")
 	case "linux":
-		platform = "linux"
+		armaExecutable = strings.Join([]string{s.Path, "arma3server"}, "/")
 	}
 
-	if platform == "" {
-		log.Fatal("Your operating system doesn't support Arma")
+	armaServer := exec.Cmd{
+		Path: armaExecutable,
+		Args: args,
+		Dir:  s.Path,
 	}
-}
 
-// StartServer starts an Arma server
-func (s *Server) StartServer() error {
-
-	return nil
+	return armaServer.CombinedOutput()
 }
